e692ba6cf1ee90e6923b92d523fe5125f77a7932
Henry Ning <henryhuning@gmail.com>
2020-06-21 17:04:57 +0000

    8. Variables 17min

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-print('hello world')
+print("Examples of variables:")
+score = 0
+lives = 3
+player1_name = "Leeroy Jenkins"
+player2_name = "Gustavo Pezzi"
+print (type(score))
+
+salary = 10 / 2
+print ("Salary is "..salary)
+
+salary2 = 25 / 2
+print ("Salary is "..salary2)
+
+print("Name is "..player1_name)
