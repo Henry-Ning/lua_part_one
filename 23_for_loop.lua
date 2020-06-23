6e3ffb9979f7b5ebed474f63ff1714fcd6b907b7
Henry Ning <henryhuning@gmail.com>
2020-06-23 22:01:26 +0000

    23. The For Loop 6min

---
+++ 23_for_loop.lua
@@ -0,0 +1,9 @@
+math.randomseed(os.time())
+
+for count = 1, 500, 25 do
+  enemy_x = math.random(0, 800)
+  enemy_y = math.random(0, 600)
+  print("Enemy "..count..": ("..enemy_x..","..enemy_y..")")
+end
+
+print("Thank you, goodbye!")
