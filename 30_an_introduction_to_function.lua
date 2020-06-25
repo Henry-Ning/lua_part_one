9f22899108a8196f37d74b9bb7ae538c34ec21bc
Henry Ning <henryhuning@gmail.com>
2020-06-25 14:15:39 +0000

    30. An Introduction to Functions 14min

---
+++ 30_an_introduction_to_function.lua
@@ -0,0 +1,54 @@
+-- Function to show the menu on the screen
+function display_menu()
+--Display a menu
+  print("--------------------------------")
+  print("Welcome, "..os.date())
+  print("--------------------------------")
+  print("1.Generate random enemy position")
+  print("2.Distance from enemy to player")
+  print("3.Get angle from enemy to player")
+  print("4.Exit")
+  print("--------------------------------")
+
+end
+
+function get_distance(x1, y1, x2, y2)
+  return math.sqrt((x1 - x2)^2 + (y1 - y2)^2)
+end
+
+math.randomseed(os.time())
+
+--player position in the middle of the screen
+local player_x, player_y = 400,300
+
+local enemy_x, enemy_y = 0, 0
+
+local user_option = 0
+
+
+--Loop while user option is different from 4 (exit)
+while user_option ~=4 do
+
+  display_menu()
+
+  --read the user option from the keyboard 
+  print("Please, select your option: ")
+  user_option = io.read("*n")
+
+  if user_option == 1 then
+    enemy_x = math.random(0,800)
+    enemy_y = math.random(0,600)
+    print("New enemy pos ("..enemy_x..","..enemy_y..")")
+  end
+  if user_option == 2 then
+    local d = get_distance(enemy_x, enemy_y, player_x, player_y)
+    print("Distance from eneymy to player: "..d)
+  end
+  if user_option == 3 then
+    local a = math.atan2((enemy_y-player_y),(enemy_x-player_x))
+    local a_deg = math.deg(a)
+    print("Angle between enemy and player is: "..a_deg)
+  end
+end
+
+print("Thank you, goodbye!")
